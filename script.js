138eeb8408fe5175bab73847ba9b05b6a5aff018
ShreeHarsha-22 <149221861+ShreeHarsha-22@users.noreply.github.com>
2024-02-03 18:38:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("February 17, 2024 11:40:00").getTime();
+var countDownDate = new Date("February 17, 2024 23:40:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
